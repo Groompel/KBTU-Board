d87b58663db206b4299c69b24576044dc41f15a8
Timur Khaimovich <itima.com@gmail.com>
2020-04-09 18:54:09 +0600

    Yet Ugly profile component

--- kbtu-board/src/app/profile/profile.component.ts
+++ kbtu-board/src/app/profile/profile.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {User} from '../../assets/interfaces/user';
-import {PostsService} from '../posts.service';
+import {PostsService} from '../_services/posts.service';
 import {Post} from '../../mocks/post';
+import {User} from '../_models/models';
+import {AuthMockService} from '../_services/auth-mock.service';
 
 
 @Component({
@@ -12,20 +13,16 @@ import {Post} from '../../mocks/post';
 export class ProfileComponent implements OnInit {
 
   posts: Post[];
+  user: User;
 
-  user: User = {
-    name: 'Василий Петрович',
-    login: 'vasek2000',
-    password: 'ao#7@fD92!',
-    telegram: 'vasek2000',
-    avatar: ''
-  };
-
-  constructor(public postsService: PostsService) {
+  constructor(
+    public postsService: PostsService,
+    public authService: AuthMockService) {
   }
 
   ngOnInit(): void {
     this.postsService.getUserPosts(1).subscribe(p => this.posts = p);
+    this.authService.currentUser.subscribe(u => this.user = u);
   }
 
 }
